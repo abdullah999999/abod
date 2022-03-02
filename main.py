8c82db0db995e7b4d956367e2d90b55bf41f566f
Kamal Alhumsi <kamalalhomsi2015@gmail.com>
2022-03-02 17:34:33 +0200

    new commit trigger

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def start(message):
     keyboard.row('الطعام', 'أرقام الأفرع')
     keyboard.row('جديدنا', 'قوالب للمناسبات')
     bot.send_message(message.chat.id, 'مرحبا بك في مأكولات الشام', reply_markup=keyboard)
-    
+
 
 @bot.message_handler(func = lambda m: True)
 def send_text(message):
@@ -100,67 +100,68 @@ def send_text(message):
         bot.send_message(message.chat.id, 'أختار ما تريد :',  reply_markup=keyboard3)
     elif message.text.lower() == 'سندويشة شاورما صغيرة':
         bot.send_photo(message.chat.id, 'https://imgur.com/gallery/2TAYV', '''الأسم: سندويشة شاورما صغيرة
-مكونات: قطعة من الخبز المشروح مع الثوم والمخلل وشرائح الشاورما 
+مكونات: قطعة من الخبز المشروح مع الثوم والمخلل وشرائح الشاورما
 السعر: 2000
 بعض الناس تسميها الفشكة ''')
     elif message.text.lower() == 'مأكولات الشام(1)':
         bot.send_location(message.chat.id, 33.520175,36.298052)
         bot.send_message(message.chat.id, '''الأسم: مأكولات الشام (1)
-مكان التواجد: شارع بغداد _ عين الكرش _ جانب المشفى العربي 
+مكان التواجد: شارع بغداد _ عين الكرش _ جانب المشفى العربي
                         رقم التواصل : 2319300\ 2313245\ 2319069\ 0940840001''')
 
     elif message.text.lower() == 'مأكولات الشام(2)':
         bot.send_location(message.chat.id, 33.520175,36.298052)
         bot.send_message(message.chat.id, '''الأسم: مأكولات الشام (2)
-مكان التواجد: عين الكرش _ مقابل حلويات شيخ الحارة 
+مكان التواجد: عين الكرش _ مقابل حلويات شيخ الحارة
                         رقم التواصل : 2319300\ 2313245\ 2319069\ 0940840001''')
 
     elif message.text.lower() == 'مأكولات الشام(3)':
         bot.send_location(message.chat.id, 33.51851,36.31033)
         bot.send_message(message.chat.id, '''الأسم: مأكولات الشام (3)
-مكان التواجد: شارع بغداد _ موقف السادات   
+مكان التواجد: شارع بغداد _ موقف السادات
                         رقم التواصل : 2319300\ 2313245\ 2319069\ 0940840001''')
 
     elif message.text.lower() == 'مأكولات الشام(4)':
         bot.send_location(message.chat.id, 33.48146,36.30776)
         bot.send_message(message.chat.id, '''الأسم: مأكولات الشام (4)
-مكان التواجد: التضامن _ شارع نسرين _ مقابل مدرسة اسكندرون    
+مكان التواجد: التضامن _ شارع نسرين _ مقابل مدرسة اسكندرون
                         رقم التواصل : 2319300\ 2313245\ 2319069\ 0940840001''')
 
     elif message.text.lower() == 'مأكولات الشام(5)':
         bot.send_location(message.chat.id, 33.50412,36.31985)
         bot.send_message(message.chat.id, '''الأسم: مأكولات الشام (5)
-مكان التواجد: مفرق دويلعة _ بعد كنيسة مارلياس _ مقابل بن حسيب   
+مكان التواجد: مفرق دويلعة _ بعد كنيسة مارلياس _ مقابل بن حسيب
                         رقم التواصل : 2319300\ 2313245\ 2319069\ 0940840001''')
 
     elif message.text.lower() == 'مأكولات الشام(6)':
         bot.send_location(message.chat.id, 33.49860,36.33405)
         bot.send_message(message.chat.id, '''الأسم: مأكولات الشام (6)
-مكان التواجد:  كشكول _ بناء الفارس   
+مكان التواجد:  كشكول _ بناء الفارس
                         رقم التواصل : 2319300\ 2313245\ 2319069\ 0940840001''')
 
     elif message.text.lower() == 'مأكولات الشام(7)':
         bot.send_message(message.chat.id, '''الأسم: مأكولات الشام (7)
-مكان التواجد: المليحة _ قريبا   
+مكان التواجد: المليحة _ قريبا
                         ''')
 
     elif message.text.lower() == 'مأكولات الشام(8)':
         bot.send_message(message.chat.id, '''الأسم: مأكولات الشام (8)
-مكان التواجد: دويلعة _ قريبا   
+مكان التواجد: دويلعة _ قريبا
                         ''')
 
     elif message.text.lower() == 'مأكولات الشام(10)':
         bot.send_location(message.chat.id, 33.50025,36.31063)
         bot.send_message(message.chat.id, '''الأسم: مأكولات الشام (10)
-مكان التواجد: الصناعة _ مقابل كراجات الست   
+مكان التواجد: الصناعة _ مقابل كراجات الست
                         رقم التواصل : 2319300\ 2313245\ 2319069\ 0940840001''')
 
     elif message.text.lower() == 'مأكولات الشام(11)':
         bot.send_location(message.chat.id, 33.51485,36.35198)
         bot.send_message(message.chat.id, '''الأسم: مأكولات الشام (11)
-مكان التواجد: عين ترما _ ساحة الخزان   
+مكان التواجد: عين ترما _ ساحة الخزان
                         رقم التواصل : 2319300\ 2313245\ 2319069\ 0940840001''')
 
 
 
 bot.polling()
+# commit
